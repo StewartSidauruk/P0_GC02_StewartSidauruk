f9993eda07d1bc6264a7e03102bbf5965abf0909
StewartSidauruk <220711816@students.uajy.ac.id>
2025-05-31 20:02:54 +0700

    penambahan kondisi 0 ganda, dan cegah titik ganda

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function operasi(val){
     const lastChar = display.value.slice(-1);
     const operators = ["+", "-", "×", "÷"];
+    const digits = ['0', '1','2','3','4','5','6','7','8','9'];
 
     if (operators.includes(val)) {
         if (operators.includes(lastChar)) {
@@ -11,9 +12,43 @@ function operasi(val){
         if (display.value === "") {
             return;
         }
+
+        // Cegah operator setelah titik
+        if (lastChar === '.') {
+            return;
+        }
+
+    }
+
+    if (digits.includes(val)) {
+        const parts = display.value.split(/[\+\-×÷]/);
+        const lastNumber = parts[parts.length - 1];
+
+        if (lastNumber === '0' && val !== '0') {
+            display.value = display.value.slice(0, -1) + val;
+            return;
+        }
+
+        if (lastNumber === '0' && val === '0') {
+            return;
+        }
+    }
+
+    if (val === '.' && pengecekanDot(display.value)) {
+        return;
+    }
+
+    if (val === '.' && operators.includes(lastChar)) {
+        return;
     }
 
     document.getElementById("display").value += val
+}
+
+function pengecekanDot(displayValue) {
+    const parts = displayValue.split(/[\+\-×÷]/);
+    const lastNumber = parts[parts.length - 1];
+    return lastNumber.includes('.');
 }
 
 function bersihDisplay(){
@@ -29,5 +64,6 @@ function kalkulasi(){
     nilaiDisplay = nilaiDisplay.replace(/×/g, '*').replace(/÷/g, '/');
 
     let hasil = eval(nilaiDisplay)
+    
     document.getElementById("display").value = hasil
 }
